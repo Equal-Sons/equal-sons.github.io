2f4ba2b719c8d5e52910e08969e6ad0da640a19a
Justin Kauszler <jkauszler@gmail.com>
2026-08-25 14:32:52 -0400

    refactor: moving unused code

--- apps/www/src/navigation/app-navigation.tsx
+++ apps/www/src/navigation/app-navigation.tsx
@@ -41,34 +41,6 @@ export default function AppNavigation() {
 
 				<Route path="/blog" element={<Blog />} />
 				<Route path="/blog-details/:id" element={<BlogDetails />} />
-
-				{/* <Route path="/home-1" element={<HomeOne />} />
-				<Route path="/home-2" element={<HomeTwo />} />
-				<Route path="/home-3" element={<HomeThree />} />
-				<Route path="/home-4" element={<HomeFour />} />
-				<Route path="/home-5" element={<HomeFive />} />
-				<Route path="/home-6" element={<HomeSix />} />
-				<Route path="/home-7" element={<HomeSeven />} />
-				<Route path="/home-8" element={<HomeEight />} />
-				<Route path="/about" element={<AboutPage />} />
-				<Route path="/service" element={<ServicePage />} />
-				<Route path="/service-2" element={<ServicePageTwo />} />
-				<Route path="/service-3" element={<ServicePageThree />} />
-				<Route path="/service-details" element={<ServiceDetailsPage />} />
-				<Route path="/team" element={<TeamPage />} />
-				<Route path="/team-details/:id" element={<TeamDetailsPage />} />
-				<Route path="/pricing" element={<PricingPage />} />
-				<Route path="/faq" element={<FaqPage />} />
-				<Route path="/project" element={<Project />} />
-				<Route path="/project-2" element={<ProjectTwo />} />
-				<Route path="/project-3" element={<ProjectThree />} />
-				<Route path="/project-4" element={<ProjectFour />} />
-				<Route path="/project-5" element={<ProjectFive />} />
-				<Route path="/project-6" element={<ProjectSix />} />
-				<Route path="/project-details" element={<ProjectDetails />} />
-				<Route path="/blog" element={<Blog />} />
-				<Route path="/blog-2" element={<BlogTwo />} />
-				<Route path="/blog-details/:id" element={<BlogDetails />} /> */}
 				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 			<BackToTop />
